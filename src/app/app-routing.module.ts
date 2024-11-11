958de5293f15cc5eaa22a7a368c6066fda700037
AlejandraPacheco <alejandra.pacheco1812@gmail.com>
2024-11-11 18:50:41 -0400

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { InventarioComponent } from './pages/inventario/inventario.component';
+import { PedidosComponent } from './pages/pedidos/pedidos.component';
+import { ContactosComponent } from './pages/contactos/contactos.component';
+import { NuevoProductoComponent } from './pages/nuevo-producto/nuevo-producto.component';
+import { ActualizarProductoComponent } from './pages/actualizar-producto/actualizar-producto.component';
+import { LoginComponent } from './pages/login/login.component';
+import { PerfilUsuarioComponent } from './pages/perfil-usuario/perfil-usuario.component';
+import { RegistrarUsuarioComponent } from './pages/registrar-usuario/registrar-usuario.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent },
+  { path: 'inventario', component: InventarioComponent },
+  { path: 'pedidos', component: PedidosComponent },
+  { path: 'contactos', component: ContactosComponent },
+  { path: 'nuevo-producto', component: NuevoProductoComponent },
+  { path: 'actualizar-producto/:id', component: ActualizarProductoComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'perfil', component: PerfilUsuarioComponent },
+  { path: 'registrar-usuario', component: RegistrarUsuarioComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
